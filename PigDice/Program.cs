a14247a0c821bedf15fbcea061936cc049a9f0e8
Mitchell Combs <mitch.combs.92@gmail.com>
2018-05-30 15:56:38 -0400

    Updated new methods

--- PigDice/Program.cs
+++ PigDice/Program.cs
@@ -4,12 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PigDice
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
+namespace PigDice {
+
+	class Program {
+
+		Random rnd = new Random();
+
+		int RollDie() {
+			return rnd.Next(1,7);
+		}
+
+		void PlayPigDice() {
+			int score = 0;
+			int die = RollDie();
+
+			while(die != 1) {
+				score += die;
+				die = RollDie();
+			}
+			Console.WriteLine("Score is {0}", score);
+		}
+
+		void Run() {
+			bool Quit = false;
+			while(!Quit) {
+
+				PlayPigDice();
+
+				Console.Write("Do you want to quit? ");
+				string answer = Console.ReadLine();
+				answer = answer.ToUpper();
+				Quit = answer.StartsWith("Y");
+			}
+		}
+
+		static void Main(string[] args) {
+			(new Program()).Run();
 		}
 	}
 }
